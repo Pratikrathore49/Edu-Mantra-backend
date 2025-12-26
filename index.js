4df15e9b10e03ce56d994df2a1ee554982fe304e
Pratik Rathore <jipratik49@gmail.com>
2025-12-26 17:36:39 +0530

    fix / update CORS origin to allow all domains

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin:"https://edu-mantra-frontend.vercel.app",
+    origin:"*",
     credentials: true,
   })
 );
